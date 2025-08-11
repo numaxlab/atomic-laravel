bd22320d697337096126629994503b895a720214
Adrián P. Blunier <adrian@numax.org>
2025-08-11 11:45:07 +0200

    Summary molecule component

--- resources/views/components/molecules/summary.blade.php
+++ resources/views/components/molecules/summary.blade.php
@@ -1,14 +1,13 @@
 <article {{ $attributes->merge(['class' => 'ml-summary'])->filter(fn ($value, $key) => ! in_array($key, ['href'])) }}>
-    @if (!empty($thumbnail))
-        <a class="summary-media-wrapper" href="{{ $attributes->get('href') }}">
-            {{ $thumbnail }}
-        </a>
-    @endif
-    <h2 class="at-heading">
-        <a href="{{ $attributes->get('href') }}">
-            {{ $slot }}
-        </a>
-    </h2>
+    <a href="{{ $attributes->get('href') }}">
+        @if (!empty($thumbnail))
+            <div class="summary-media-wrapper" href="{{ $attributes->get('href') }}">
+                {{ $thumbnail }}
+            </div>
+        @endif
+
+        {{ $slot }}
+    </a>
     @if (!empty($content))
         <div class="summary-content">
             {{ $content }}
